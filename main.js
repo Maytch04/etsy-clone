20959721b9f0d40c794965808e3c2c50e218d61c
Martinez Aytch <2kproductionss@gmail.com>
2017-03-19 22:15:41 -0700

    final commit

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
+$(document).ready(function() {
+    $.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data) {
 
-$(document).ready(function(){
-	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
-		
-		var data = data.results
-		// ${data[0].Images[0].url_570xN	 
+        var data = data.results
+            // ${data[0].Images[0].url_570xN	 
 
-	function addImageContainer(){
-	var htmlStr = ""
-	for (var i = 0; i < data.length; i++) {
-	htmlStr += `<div class="productImage" style="background-image:url('${data[i].Images[0].url_570xN}'); background-size: 260px 266px;
-    background-position-y: -14px; background-repeat: no-repeat;">
+        function addImageContainer() {
+            var htmlStr = ""
+            for (var i = 0; i < data.length; i++) {
+                htmlStr += `<a href="#"><div class="productImage"  style="background-image:url('${data[i].Images[0].url_570xN}'); background-size: 260px 266px;
+	    background-position-y: -14px; background-repeat: no-repeat;"></a><a href="#"><div class="love"></div></a><a href="#"><div class="hamb" ></div></a>
 				<div class="productInfo">
 					<ul>
 						 <li>${data[i].description}</li>
@@ -18,19 +17,10 @@ $(document).ready(function(){
 					</ul>
 				</div>
 			</div>`
-		}
-	$('.images').html(htmlStr)
-	}
-addImageContainer()
+            }
+            $('.images').html(htmlStr)
+        }
+        addImageContainer()
 
-		})
-	})
-
-	
-	
-
-
-
-
-	
-
+    })
+})
